615e3adb3a6be2b30ab891a71f41ab9ea47ffac2
Alfred215 <alfredoarpal@gmail.com>
2022-04-25 12:56:15 +0200

    son las 12:56 del 25/04

--- es.efor.OnBoarding.Angular/proyecto-ionic/src/app/tab1/tab1.page.ts
+++ es.efor.OnBoarding.Angular/proyecto-ionic/src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ViewChild  } from '@angular/core';
+import { TeamDto } from '../shared/api/models';
+import { TeamService } from '../shared/api/services';
 
 @Component({
   selector: 'app-tab1',
@@ -6,6 +8,13 @@ import { Component, OnInit, ViewChild  } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page implements OnInit{
+
+  item: TeamDto={
+    id: 0,
+    name: '',
+    league: '',
+    active: true
+  };
 
   constructor() {}
 
